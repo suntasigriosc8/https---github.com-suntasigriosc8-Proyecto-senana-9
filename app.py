f63aeba52240b7baa1eed9165bdc8b863a0aeb00
suntasigrios <suntasigrriosc8@gmail.com>
2025-08-24 17:25:43 -0500

    Proyecto Flask con plantillas dinámicas

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/usuarios/<nombre>')
 def usuarios(nombre):
